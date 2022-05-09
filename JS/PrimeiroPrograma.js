562a5a44e8a13a312808b40edd15e3b92a0f21e4
JoaoClaudioSG <96254615+JoaoClaudioSG@users.noreply.github.com>
2022-05-09 19:39:05 -0300

    att

--- JS/PrimeiroPrograma.js
+++ JS/PrimeiroPrograma.js
@@ -1,3 +1,8 @@
 console.log ("Meu primeiro programa: Trabalahndo com variaveis");
 
 const idade = 24;
+const idadeSomada = 24+4
+
+console.log (idade)
+console.log (idade+2)
+console.log (idadeSomada)
